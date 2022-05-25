753af005f80b8ab15ed7bf096ebeaf7841956e8d
HEMANTH510015 <HEMANTH.I@CAPGEMINI.COM>
2022-05-25 13:48:44 +0530

    updated

--- credit-card-system/lib/src/main/java/com/cg/creditcardsystem/Sample.java
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/Sample.java
@@ -3,8 +3,8 @@ package com.cg.creditcardsystem;
 public class Sample {
 
 	public static void main(String[] args) {
-  System.out.println("hello....");
-  System.out.println("hii....");
+    System.out.println("hello....");
+    System.out.println("hii....");
 	}
 
 }
