73132b9d256bccf11e496ebbe9b0c4c68e549bc0
HEMANTH510015 <HEMANTH.I@CAPGEMINI.COM>
2022-05-25 12:07:33 +0530

    Update Sample.java

--- credit-card-system/lib/src/main/java/com/cg/creditcardsystem/Sample.java
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/Sample.java
@@ -3,8 +3,8 @@ package com.cg.creditcardsystem;
 public class Sample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
   System.out.println("hello....");
+  System.out.println("hii....");
 	}
 
 }
